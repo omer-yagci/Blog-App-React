604b9241119bc7f1f119c4b690c9d76b2c141d11
khan <omeryagci3295@gmail.com>
2022-11-16 18:48:44 +0300

    approuter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import AppRouter from "./router/AppRouter";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div className="App">
-      <h1>Bismillah</h1>
+      <AppRouter />
+      <ToastContainer />
     </div>
   );
 }
