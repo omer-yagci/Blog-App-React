1cb99ff0e281c7673c1bc1a133afc5065f7bc2c5
khan <omeryagci3295@gmail.com>
2022-11-17 15:36:24 +0300

    new pages added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,6 +5,8 @@ import Register from "../pages/Register";
 import DashBoard from "../pages/Dashboard";
 import Navbar from "../components/Navbar";
 import About from "../pages/About";
+import NewBlog from "../pages/NewBlog";
+import UpdateBlog from "../pages/UpdateBlog";
 
 const AppRouter = () => {
   return (
@@ -15,6 +17,8 @@ const AppRouter = () => {
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="about" element={<About />} />
+        <Route path="newblog" element={<NewBlog />} />
+        <Route path="updateblog" element={<UpdateBlog />} />
       </Routes>
     </BrowserRouter>
   );
