c6838deaf0e5db983f065436ffb7637e52869dd3
khan <omeryagci3295@gmail.com>
2022-11-18 18:45:02 +0300

    data maplanebiliyor

---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BlogCard = ({ element }) => {
+  return <div>BlogCard</div>;
+};
+
+export default BlogCard;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 import { useFetch } from "../auth/functions";
+import BlogCard from "../components/BlogCard";
 
 const DashBoard = () => {
   const { isLoading, contactList } = useFetch();
   return (
     <div>
-      <h1>DashBoard</h1>
+      {/* <h1>DashBoard</h1> */}
       {isLoading && <h1>loading</h1>}
+      {contactList?.length === 0 && <h1>data mata yok </h1>}
       {!isLoading &&
-        contactList?.map((item, index) => {
-          return (
-            <h1>
-              {item.title}
-              {item.image}
-              {item.contact}
-            </h1>
-          );
+        contactList?.map((element, index) => {
+          return <BlogCard key={index} element={element} />;
         })}
     </div>
   );
