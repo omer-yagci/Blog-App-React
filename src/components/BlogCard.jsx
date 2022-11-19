5aa00c4640429bc11e33ab50b1fa75055723ae21
khan <omeryagci3295@gmail.com>
2022-11-19 12:47:55 +0300

    blog card

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 const BlogCard = ({ element }) => {
-  return <div>BlogCard</div>;
+  console.log(element);
+  const { image, content, title } = element;
+  return (
+    <Card sx={{ maxWidth: 345, margin: "1rem" }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={<IconButton aria-label="settings"></IconButton>}
+        title={title}
+        subheader="September 14, 2016"
+      />
+      <CardMedia component="img" height="100" image={image} alt="Paella dish" />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {content}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default BlogCard;
