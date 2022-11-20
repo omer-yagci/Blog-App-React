f5f9267fa48a361be61dac3b76525acb5f7276c5
khan <omeryagci3295@gmail.com>
2022-11-20 15:26:01 +0300

    navbar done !

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,18 +58,8 @@ export default function MenuAppBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Photos
-            <h1>{displayName}</h1>
+            <h1 className="text-2xl uppercase">{displayName}</h1>
           </Typography>
           {
             <div>
