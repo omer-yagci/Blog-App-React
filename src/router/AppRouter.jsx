f0bec168b04ae98dc111987ef321dd3a35347d36
khan <omeryagci3295@gmail.com>
2022-11-22 18:02:41 +0300

    private route done!

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,9 +6,9 @@ import DashBoard from "../pages/Dashboard";
 import Navbar from "../components/Navbar";
 import About from "../pages/About";
 import NewBlog from "../pages/NewBlog";
-import UpdateBlog from "../pages/UpdateBlog";
 import PrivateRouter from "./PrivateRouter";
 import Details from "../pages/Details";
+import ErrorPage from "../pages/ErrorPage";
 
 const AppRouter = () => {
   return (
@@ -21,14 +21,15 @@ const AppRouter = () => {
         <Route path="/newblog" element={<PrivateRouter />}>
           <Route path="" element={<NewBlog />} />
         </Route>
-        <Route path="/updateblog" element={<PrivateRouter />}>
-          <Route path="" element={<UpdateBlog />} />
-        </Route>
-
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/about" element={<About />} />
-        <Route path="/details" element={<Details />} />
+
+        <Route path="/details" element={<PrivateRouter />}>
+          <Route path="" element={<Details />} />
+        </Route>
+
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
