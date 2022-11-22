42c66967089e26e1df0743e0e3163853e2b17b60
khan <omeryagci3295@gmail.com>
2022-11-22 15:56:22 +0300

    tailwind v.01

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: [
+    "./src/**/*.{html,js,jsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
+  plugins: [require("tw-elements/dist/plugin")],
 };
